de848f5647cf3e05f073f4e0ef83ee6c5b3b3194
valenpy22 <valentina.campos.o@usach.cl>
2023-09-28 08:44:22 -0300

    fix: fixing the main html

--- preuniversitario/src/main/java/com/example/preuniversitario/controllers/FeeController.java
+++ preuniversitario/src/main/java/com/example/preuniversitario/controllers/FeeController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping
@@ -16,5 +19,11 @@ public class FeeController {
     @GetMapping("/uploadFee")
     public String newFee(){
         return "uploadFee";
+    }
+
+    @PostMapping("/saveFee")
+    public String saveFee(@RequestParam Map<String, String> allParameters){
+        feeService.saveFee(allParameters);
+        return "redirect:/saveFee";
     }
 }
