96029b4297bf6897dde58bbf9bdb4258ef2f2717
valenpy22 <valentina.campos.o@usach.cl>
2023-10-07 20:12:40 -0300

    feat: adding payment tests

---
+++ preuniversitario/src/test/java/com/example/preuniversitario/PaymentTest.java
@@ -0,0 +1,77 @@
+package com.example.preuniversitario;
+
+import com.example.preuniversitario.entities.PaymentEntity;
+import com.example.preuniversitario.entities.ReportSummaryEntity;
+import com.example.preuniversitario.entities.StudentEntity;
+import com.example.preuniversitario.entities.UploadDataEntity;
+import com.example.preuniversitario.repositories.PaymentRepository;
+import com.example.preuniversitario.repositories.ReportSummaryRepository;
+import com.example.preuniversitario.repositories.StudentRepository;
+import com.example.preuniversitario.repositories.UploadDataRepository;
+import com.example.preuniversitario.services.PaymentService;
+import com.example.preuniversitario.services.ReportSummaryService;
+import com.example.preuniversitario.services.StudentService;
+import com.example.preuniversitario.services.UploadDataService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class PaymentTest {
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    UploadDataRepository uploadDataRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    ReportSummaryRepository reportSummaryRepository;
+
+    @Test
+    void calculateDiscountsTest(){
+        PaymentEntity payment = new PaymentEntity();
+        StudentEntity student = new StudentEntity();
+        UploadDataEntity uploadData = new UploadDataEntity();
+        ReportSummaryEntity reportSummary = new ReportSummaryEntity();
+
+        student.setRut("21305689-1");
+        student.setSchool_type("Municipal");
+        student.setSenior_year(2021);
+        uploadData.setRut("21305689-1");
+        reportSummary.setRut(student.getRut());
+        studentRepository.save(student);
+        uploadDataRepository.save(uploadData);
+        reportSummaryRepository.save(reportSummary);
+
+        payment.setRut("21305689-1");
+        payment.setInterest_months_late(10000);
+        payment.setDiscount_senior_year(10000);
+        payment.setDiscount_school_type(10000);
+        payment.setDiscount_average_score(10000);
+        payment.setTotal_price(1000000);
+
+        paymentRepository.save(payment);
+
+        ArrayList<PaymentEntity> arrayList = paymentService.calculateDiscounts();
+
+        //CAMBIAR PARA EL DÍA DE LA PRUEBA
+        assertNull(arrayList);
+        paymentRepository.deleteAll();
+    }
+
+    @Test
+    void getAllDiscountsTest(){
+        ArrayList<PaymentEntity> paymentEntities = paymentService.getAllDiscounts();
+        assertEquals(new ArrayList<>(), paymentEntities);
+    }
+}
